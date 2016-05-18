6b360e81a521f9f30394f8c2a48c0f71d9136c86
David Kirk <dbkirk4211@gmail.com>
2016-05-18 03:27:26 -0400

    LoadListMethod; spObjectManager Service; formatting/bugs

--- dk-ng-sharepoint.js
+++ dk-ng-sharepoint.js
@@ -1,27 +1,69 @@
 angular.module("ng-dk-sharepoint", [])
-.service("SP", ["$http", "$q", "$interval", function($http, $q, $interval){
-  var SP = this;
-  
-  SP.host_base = null;
-  SP.request_digest = null;
-  
-  (function(deferred){
-    SP.initialized = deferred.promise;
-    
-    var promise = $interval(function(){
+.service("SP", ["$http", "$q", "$interval", "spObjectManager", function($http, $q, $interval, spObjectManager){
+	var SP = this;
+	
+	SP.host_base = null;
+	SP.request_digest = null;
+	
+	(function(deferred){
+		SP.initialized = deferred.promise;
+		
+		var promise = $interval(function(){
 			if($scope.test = angular.element("#__REQUESTDIGEST").val()){
-			  $interval.cancel(promise);
-			  deferred.resolve(SP);
-    },0,0,false)
-  })($q.defer())
-  
-  SP.request = function(request){
-    return SP.initialized.then(function(){
-      request.headers = request.headers || {};
-      request.headers["X-RequestDigest"] = SP.request_digest;
-      request.headers["accept"] = "application/json; odata=verbose";
-      
-      return $http(request);
-    })
-  }
+				$interval.cancel(promise);
+				deferred.resolve(SP);
+			}
+		},0,0,false)
+	})($q.defer())
+	
+	SP.request = function(request){
+		return SP.initialized.then(function(){
+			request.headers = request.headers || {};
+			request.headers["X-RequestDigest"] = SP.request_digest;
+			request.headers["accept"] = "application/json; odata=verbose";
+			
+			return $http(request);
+		})
+	}
+	
+	SP.loadList = function(api_url){
+		return SP.request({
+			url: api_url
+		}).then(function(response){
+			return spObjectManager.createList(response.data.d);
+		})
+	}
 }])
+
+.service("spObjectManager", function(){
+	var ObjectManager = this;
+  	
+  	var List = function(response){
+  		var List = this;
+  		
+  		List.id = response.Id;
+  		List.api = response.__metadata.uri;
+  	}
+  	
+  	List.prototype.update = function(response){
+  		var List = this;
+  		
+  		List.name = response.Title;
+  	}
+  	
+  	List.prototype.reload = function(){
+  		var List = this;
+  		
+  		return SP.response({
+  			url: List.api
+  		}).then(function(response){
+  			List.update(response);
+  			
+  			return List;
+  		})
+  	}
+  	
+  	ObjectManager.createList = function(response){
+  		return new List(response);
+  	}
+})
